0298c050e29dd93da2b4af8246417085e47fbd52
y4c6 <syc109072510@gapp.nthu.edu.tw>
2022-10-19 17:10:56 +0800

    yourmessage

---
+++ DM2022-Lab1-Homework-main/helpers/data_mining_helpers.py
@@ -0,0 +1,26 @@
+import nltk
+
+"""
+Helper functions for data mining lab session 2018 Fall Semester
+Author: Elvis Saravia
+Email: ellfae@gmail.com
+"""
+
+def check_missing_values(row):
+    """ functions that check and verifies if there are missing values in dataframe """
+    counter = 0
+    for element in row:
+        if element == True:
+            counter+=1
+    return ("The amoung of missing records is: ", counter)
+
+def tokenize_text(text, remove_stopwords=False):
+    """
+    Tokenize text using the nltk library
+    """
+    tokens = []
+    for d in nltk.sent_tokenize(text, language='english'):
+        for word in nltk.word_tokenize(d, language='english'):
+            # filters here
+            tokens.append(word)
+    return tokens
